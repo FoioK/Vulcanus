af623507da25f201a10746223e87e18c7bb3acf7
Wojciech Koziol <7wojok7@gmail.com>
2018-03-17 14:44:04 +0100

    Reduce queues to one queue. Change app setup. Use only workers.

--- src/main/java/com/wojo/Vulcanus/Queue.java
+++ src/main/java/com/wojo/Vulcanus/Queue.java
@@ -1,5 +1,6 @@
 package com.wojo.Vulcanus;
 
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -7,7 +8,5 @@ public class Queue {
 
     private static final Integer QUEUE_SIZE = 1024;
 
-    public static final BlockingQueue<Pair<String, String>> URL_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
-    public static final BlockingQueue<Pair<String, byte[]>> REQUEST_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
-    public static final BlockingQueue<Pair<String, String>> ANSWERS = new ArrayBlockingQueue<>(QUEUE_SIZE);
+    public static final BlockingQueue<Request<String, List<String>>> REQUEST_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
 }
--- src/main/java/com/wojo/Vulcanus/Setup.java
+++ src/main/java/com/wojo/Vulcanus/Setup.java
@@ -4,9 +4,8 @@ public class Setup implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            new Thread(new Producer(Queue.URL_QUEUE, Queue.REQUEST_QUEUE)).start();
-        }
-        new Thread(new Worker(Queue.REQUEST_QUEUE, Queue.ANSWERS)).start();
+        new Thread(new Worker(Queue.REQUEST_QUEUE)).start();
+        new Thread(new Worker(Queue.REQUEST_QUEUE)).start();
+        new Thread(new Worker(Queue.REQUEST_QUEUE)).start();
     }
 }
